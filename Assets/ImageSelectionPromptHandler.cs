c2a98ff9a7e9c86ff7a6f351a091031994088c64
Michon Bojador <mbojador@gmail.com>
2025-09-20 23:17:05 +0400

    Applied Selection Behaviour Frontend

--- Assets/ImageSelectionPromptHandler.cs
+++ Assets/ImageSelectionPromptHandler.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ImageSelectionPromptHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform selectionTransform;
+    public Transform[] selectPositions = new Transform[3];
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            MoveSelection(0);
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            MoveSelection(1);
+        }
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            MoveSelection(2);
+        }
+    }
+
+    public void MoveSelection(int index)
+    {
+        if (index < 0 || index >= selectPositions.Length) return;
+        selectionTransform.position = selectPositions[index].position;
     }
 }
